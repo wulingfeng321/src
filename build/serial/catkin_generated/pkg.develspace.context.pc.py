e6dad50936cadeb4539a2821f96ed0198821f0cf
wulingfeng <hhcchh2005@126.com>
2024-12-05 00:44:05 +0800

    去掉了 src下的build文件夹

--- build/serial/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/wulingfeng/catkin_ws/src/serial/include".split(';') if "/home/wulingfeng/catkin_ws/src/serial/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lserial;-lrt;-lpthread".split(';') if "-lserial;-lrt;-lpthread" != "" else []
-PROJECT_NAME = "serial"
-PROJECT_SPACE_DIR = "/home/wulingfeng/catkin_ws/src/build/devel"
-PROJECT_VERSION = "1.2.1"
